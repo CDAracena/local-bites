53b06de670b19d46e945b54db12bb16a1037b546
Christian Aracena <CDAracena@gmail.com>
2018-10-20 18:52:04 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import './index.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+console.log(axios);
+
+
+class App extends React.Component{
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return(
+      <div className="app-container">
+        <div className="app-title h1 text-center">
+          Local Bites
+        </div>
       </div>
-    );
+    )
   }
 }
 
