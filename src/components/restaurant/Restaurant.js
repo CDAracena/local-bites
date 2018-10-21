2e407c976e289ac48a4a78b28b0d207418e9fda7
Christian Aracena <CDAracena@gmail.com>
2018-10-21 02:15:58 -0400

    started adding css to individual restaurant card components

--- src/components/restaurant/Restaurant.js
+++ src/components/restaurant/Restaurant.js
@@ -14,23 +14,22 @@ const Restaurant = ({
       {name}
     </div>
   </div>
-  <div className="row">
-    <div className="col-lg-6">
+  <div className="row price-rating-row">
+    <div className="col-lg-6 price-div col-sm-6 col-xs-6">
       {price}
     </div>
-    <div className="col-lg-6">
-      {rating}
+    <div className="col-lg-6 col-sm-6 col-xs-6 rating-div">
+      Rating: {rating}
     </div>
   </div>
   <div className="row">
-    <div className="col-lg-12">
+    <div className="col-lg-12 link-row">
       <a href={yelpLink}>
-        {name}
-        on Yelp
+      {name}<img src="https://s3-media2.fl.yelpcdn.com/assets/srv0/styleguide/96b838280c20/assets/img/brand_guidelines/dont_rearrange.png" width="25%" alt="yelp logo"/>
       </a>
     </div>
   </div>
-  <div className="row">
+  <div className="row status-phone-row">
     <div className="col-lg-6">
       {open}
     </div>
